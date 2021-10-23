566e6b4bea3235d47272b62d5e6f58c19c33c244
Matthew Whited <matt@whited.us>
2021-10-23 07:39:32 -0400

    fresh start

---
+++ src/CameraColorTools/WpfApp1/ViewModel/ColorViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace WpfApp1.ViewModel
+{
+    public class ColorViewModel : ViewModelBase
+    {
+        public ColorViewModel()
+        {
+            RGB = new RgbViewModel(this);
+            HSV = new HsvViewModel(this);
+            HSL = new HslViewModel(this);
+            RYB = new RybViewModel(this);
+            CMYK = new CmykViewModel(this);
+            CMY = new CmyViewModel(this);
+
+            this.RGB.UpdateOthers();
+        }
+
+        public RgbViewModel RGB { get; }
+        public HsvViewModel HSV { get; }
+        public HslViewModel HSL { get; }
+        public RybViewModel RYB { get; }
+        public CmykViewModel CMYK { get; }
+        public CmyViewModel CMY { get; }
+    }
+}
